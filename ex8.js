0b1f5516a6d7eebdea632e1f7f6841d515da5ef5
aleixnit <nitjac210@gmail.com>
2023-05-04 10:13:40 +0200

    ejercicio 7 sort method && ejercicio 8 findIndex/ slice exercice

--- ex8.js
+++ ex8.js
@@ -2,8 +2,20 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
 
 function cortaPorAqui(corteIzq, corteDer, array) {
-    return;
+    const indexIzq = array.findIndex(animal => animal === corteIzq);
+  const indexDer = array.findIndex(animal => animal === corteDer);
+  
+  if (indexIzq === -1 || indexDer === -1) {
+    // Uno o ambos elementos no se encontraron en el array
+    return [];
+  }
+  
+  const start = Math.min(indexIzq, indexDer);
+  const end = Math.max(indexIzq, indexDer) + 1;
+  
+  return array.slice(start, end);
 }
+
 
 /**
  * Utiliza de manera combinada y adecuadamente el método .findIndex y .slice para 'cortar' un array entre dos elementos. Para simplificar, el primer animal siempre está 'a la izquierda' del segundo animal por el que queremos cortar
